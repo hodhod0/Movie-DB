de5a5d5498778f9ec907bedce42d49a03026b76f
Hodhod <alameddinehodhod@hotmail.com>
2021-12-30 12:23:41 +0200

    step 3

--- index.js
+++ index.js
@@ -4,5 +4,18 @@ const port = 3000;
 
 app.get("/", (req, res) => res.send("Ok"));
 app.listen(port, ()=>{
+
     console.log('server is  at http://localhost:3000')
+})
+
+
+app.get("/test", (req, res)=>{
+    res.send({status:200, message:"Ok"})
+})
+app.get("/time", (req, res)=>{
+    const date=new Date();
+var hour = date.getHours();
+
+var min = ('0'+date.getMinutes().toString()).slice(-2);
+    res.send(`{status:200, message:${hour}:${min}`)
 })
