bddc9ba7dec1ea1f0bf5127b8623dfd62c58fa7c
Hodhod <alameddinehodhod@hotmail.com>
2022-01-03 15:03:17 +0200

    step 7

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const app = express();
 const port = 3000;
 
 const movies = [
-    { title: 'Jaws', year: 1975, rating: 8 },
-    { title: 'Avatar', year: 2009, rating: 7.8 },
-    { title: 'Brazil', year: 1985, rating: 8 },
-    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
-]
+  { title: "Jaws", year: 1975, rating: 8 },
+  { title: "Avatar", year: 2009, rating: 7.8 },
+  { title: "Brazil", year: 1985, rating: 8 },
+  { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 },
+];
 
 app.get("/", (req, res) => res.send("Ok"));
-
 
 app.get("/test", (req, res) => {
   res.send({ status: 200, message: "Ok" });
@@ -32,7 +31,7 @@ app.get("/hello/:id", (req, res) => {
 
 app.get("/search", (req, res) => {
   const nor = req.query;
-  
+
   if (nor.s != "") {
     res.send({
       status: 200,
@@ -48,39 +47,59 @@ app.get("/search", (req, res) => {
   }
 });
 
-app.get('/movies/add', (req, res)=>{
-    res.send();
-})
+app.get("/movies/add", (req, res) => {
+  res.send();
+});
 
-app.get('/movies/get', (req, res)=>{
-    res.send({
-        status:200,
-        data: movies,
-    });
-})
+app.get("/movies/read/id/:id", (req, res) => {
+  const id=req.params.id;
+  const movie=movies.find(x=>x.title===id);
+  if(movie)
+  {res.send({
+    status: 200,
+    data: movie,
+  });
+}else{
+    res.status(404).send({status:404, error:true, message:`the movie ${id} does not exist`})
+  }
+});
 
-app.get('/movies/get/by-title', (req, res) => {
-    res.send({status:200, data:movies.sort(function(a, b){return a.title - b.title}),})
-})
-app.get('/movies/get/by-rating', (req, res) => {
-    res.send({status:200, data:movies.sort(function(a, b){return b.rating - a.rating}),})
-})
+app.get("/movies/get/by-title", (req, res) => {
+  res.send({
+    status: 200,
+    data: movies.sort(function (a, b) {
+      return a.title - b.title;
+    }),
+  });
+});
+app.get("/movies/get/by-rating", (req, res) => {
+  res.send({
+    status: 200,
+    data: movies.sort(function (a, b) {
+      return b.rating - a.rating;
+    }),
+  });
+});
 
-app.get('/movies/get/by-date', (req, res) => {
-    res.send({status:200, data:movies.sort(function(a, b){return b.year - a.year}),})
-})
+app.get("/movies/get/by-date", (req, res) => {
+  res.send({
+    status: 200,
+    data: movies.sort(function (a, b) {
+      return b.year - a.year;
+    }),
+  });
+});
 
-app.get('/movies/edit', (req, res)=>{
-    res.send();
-})
-app.get('/movies/delete', (req, res)=>{
-    res.send();
-})
-
+app.get("/movies/edit", (req, res) => {
+  res.send();
+});
+app.get("/movies/delete", (req, res) => {
+  res.send();
+});
 
 app.listen(port, () => {
-    console.log(`server is  at http://localhost:${port}`);
-  });
+  console.log(`server is  at http://localhost:${port}`);
+});
 
 // app.use((req, res) => {
 //   res.status(404).send("Sorry can't find that!");
